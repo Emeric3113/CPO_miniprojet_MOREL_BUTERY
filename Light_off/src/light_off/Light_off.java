1854f14b62e690751ad5ed8c41a9e0bcacfb0a69
emeri <emeri@10.3.60.19>
2024-11-20 14:48:30 +0100

    Création du projet

---
+++ Light_off/src/light_off/Light_off.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package light_off;
+
+/**
+ *
+ * @author emeri
+ */
+public class Light_off {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
