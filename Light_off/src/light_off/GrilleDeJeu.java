0bc106ef0512e1549ffff4492d4573e4061fa820
emeri <emeri@10.3.60.19>
2024-11-29 14:50:20 +0100

    Remplissage class GrilleDeJeu

--- Light_off/src/light_off/GrilleDeJeu.java
+++ Light_off/src/light_off/GrilleDeJeu.java
@@ -4,10 +4,71 @@
  */
 package light_off;
 
+import java.util.Random;
+
 /**
  *
  * @author emeri
  */
 public class GrilleDeJeu {
-    
+    private CelluleLumineuse[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+
+    public GrilleDeJeu(int nbLignes, int nbColonnes) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                this.matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
+
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < nbColonnes; j++) {
+            this.matriceCellules[idLigne][j].activerCellule();
+        }
+    }
+
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < nbLignes; i++) {
+            this.matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+
+    public void melangerMatriceAleatoirement(int nbTours) {
+        Random random = new Random();
+        for (int t = 0; t < nbTours; t++) {
+            if (random.nextBoolean()) {
+                activerLigneDeCellules(random.nextInt(nbLignes));
+            } else {
+                activerColonneDeCellules(random.nextInt(nbColonnes));
+            }
+        }
+    }
+
+    public boolean cellulesToutesEteintes() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if (!this.matriceCellules[i][j].estEteint()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                sb.append(this.matriceCellules[i][j].toString()).append(" ");
+            }
+            sb.append("\\n");
+        }
+        return sb.toString();
+    }
 }
