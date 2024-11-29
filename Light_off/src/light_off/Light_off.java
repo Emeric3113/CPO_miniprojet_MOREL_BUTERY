7454754e5428170de948d3f42365cea1a2a357f4
Maixent Butery <maixent.butery20@gmail.com>
2024-11-29 16:06:30 +0100

    Remplissage de Light_off

--- Light_off/src/light_off/Light_off.java
+++ Light_off/src/light_off/Light_off.java
@@ -14,8 +14,8 @@ public class Light_off {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        Partie partie = new Partie(5, 5); // Grille 5x5 par défaut
+        partie.initialiserPartie(10); // Mélange avec 10 activations aléatoires
+        partie.lancerPartie(); // Démarrer la partie
     }
-    
-    
 }
