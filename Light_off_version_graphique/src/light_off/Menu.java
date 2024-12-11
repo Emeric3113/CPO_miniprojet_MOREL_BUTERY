f174b763c01bce868c1574f2be8a951abcc7a722
Maixent Butery <maixent.butery20@gmail.com>
2024-12-11 14:22:37 +0100

    Ajout de la fermeture du menu après séléction

--- Light_off_version_graphique/src/light_off/Menu.java
+++ Light_off_version_graphique/src/light_off/Menu.java
@@ -92,6 +92,7 @@ public class Menu extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
 new PartieGUI();
+this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
